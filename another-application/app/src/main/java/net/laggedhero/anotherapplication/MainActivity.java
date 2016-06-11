b0a9db1945ea00712a93125cab9a5bceb3e6c558
Tiago Cunha <tiagocunha@gmail.com>
2016-06-11 11:34:11 -0300

    Google sign in

--- another-application/app/src/main/java/net/laggedhero/anotherapplication/MainActivity.java
+++ another-application/app/src/main/java/net/laggedhero/anotherapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package net.laggedhero.anotherapplication;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
@@ -12,13 +15,27 @@ import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String TAG = "MainActivity";
+
+    private static final int GOOGLE_SIGN_IN = 1200;
 
     private CallbackManager callbackManager;
     private ProfileTracker profileTracker;
 
-    private LoginButton loginButton;
+    private GoogleApiClient googleApiClient;
+
     private TextView nameTextView;
 
     @Override
@@ -28,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         callbackManager = CallbackManager.Factory.create();
 
-        loginButton = (LoginButton) findViewById(R.id.loginButton);
+        final LoginButton loginButton = (LoginButton) findViewById(R.id.facebookLoginButton);
         if (loginButton != null) {
             loginButton.setReadPermissions("email", "public_profile");
             loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -50,24 +67,53 @@ public class MainActivity extends AppCompatActivity {
         }
 
         nameTextView = (TextView) findViewById(R.id.nameTextView);
-
-        profileTracker = new ProfileTracker() {
-            @Override
-            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
-                if (nameTextView != null) {
+        if (nameTextView != null) {
+            profileTracker = new ProfileTracker() {
+                @Override
+                protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
                     if (currentProfile != null) {
                         nameTextView.setText(currentProfile.getName());
                     } else {
                         nameTextView.setText(":(");
                     }
                 }
-            }
-        };
+            };
+        }
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.googleLoginButton);
+        if (signInButton != null) {
+            signInButton.setSize(SignInButton.SIZE_STANDARD);
+            signInButton.setScopes(gso.getScopeArray());
+
+            signInButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                    startActivityForResult(signInIntent, GOOGLE_SIGN_IN);
+                }
+            });
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+            return;
+        }
+
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
@@ -76,5 +122,36 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
 
         profileTracker.stopTracking();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        //
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            if (acct != null && nameTextView != null) {
+                nameTextView.setText(acct.getDisplayName());
+            }
+        } else {
+            if (nameTextView != null) {
+                nameTextView.setText(":((");
+            }
+        }
+    }
+
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if (nameTextView != null) {
+                    nameTextView.setText(":(((");
+                }
+            }
+        });
     }
 }
